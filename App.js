c8350b4354df23004339825f4124087aab31d210
12rgilang <114050735+12rgilang@users.noreply.github.com>
2022-09-26 21:32:08 +0700

    Add files via upload

---
+++ App.js
@@ -0,0 +1,27 @@
+import logo from './logo.svg';
+import './App.css';
+import Item from './MyItem';
+
+// working with React.js click events
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <Item courses="Udemy"/>
+        <Item courses="Purwadhika"/>
+        <Item courses="Ruang Guru"/>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
---
+++ MyItem.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+
+// working with React.js click events
+class Item extends React.Component{
+
+    clickMe() {
+        console.log("I was clicked")
+    }
+
+    render() {
+        return(
+            // (=> to executed) "()events"
+            <h5 onClick={() => this.clickMe()}>Heloo iam Gilang Ramadhan i learning this courses via {this.props.courses}</h5>
+        )
+    }
+}
+
+export default Item;
